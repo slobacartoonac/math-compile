5995caad977ec8e8f1b6c91f64f3358559293c8a
Slobodan <slobacartoonac@gmail.com>
2023-03-05 13:57:50 +0100

    Try ts

--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { parseProgram } from "./parser";
-import { evaluate, Parameters } from "./interpreter";
-declare function execute(program: string, params: Parameters): {
-    res: number;
-    err: string;
-};
-export { execute, evaluate, parseProgram };
--- build/interpreter.d.ts
+++
@@ -1,9 +0,0 @@
-import { Exp } from "./parser";
-export interface Parameters {
-    [key: string]: number;
-}
-export declare class NegateExp extends Exp {
-    expression: Exp;
-    constructor(expression: Exp);
-}
-export declare function evaluate(expression: Exp, parameters?: Parameters): number;
